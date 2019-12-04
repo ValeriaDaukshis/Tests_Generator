1ffb46db953d8d48a0a07a2776aa3b75606df708
Valeria <daukshislera2000@gmail.com>
2019-12-04 15:45:14 +0300

    create EnterPoint class

---
+++ EnterPoint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using GeneratorLib;
+
+namespace EnterPoint
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            PipelineConfiguration configuration = new PipelineConfiguration(3, 3, 9);
+            List<string> fileNames = new List<string>();
+            fileNames.Add("TestClass1.txt");
+            fileNames.Add("TestClass2.txt");
+            
+            Pipeline generator = new Pipeline(configuration);
+            string outputFileName = "Output.txt";
+            generator.CreatePipeline(fileNames, outputFileName).Wait();
+            Console.WriteLine($"The generated tests was created");
+        }
+    }
+}
