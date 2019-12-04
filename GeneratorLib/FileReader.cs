dee772ebfd3c1fec48d5d5e555a398874dc040e5
Valeria <daukshislera2000@gmail.com>
2019-12-04 12:23:41 +0300

    create filewriter and filereader classes

---
+++ GeneratorLib/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace GeneratorLib
+{
+    public class FileReader
+    {
+        public static async Task<string> Read(string fileName)
+        {
+            using (var reader = new StreamReader(new FileStream(fileName, FileMode.Open)))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+}
---
+++ GeneratorLib/FileWriter.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace GeneratorLib
+{
+    public class FileWriter
+    {
+        public static async void Write(string fileName, string text)
+        {
+            using (var writer = new StreamWriter(new FileStream(fileName, FileMode.OpenOrCreate)))
+            {
+                await writer.WriteAsync(text);
+            }
+            
+        }
+    }
+}
